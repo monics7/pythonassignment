722b7952fda5d2c60351729b3c87336af8b6e760
Monica Maibram <maibram.monica06@gmail.com>
2020-05-02 13:41:15 +0530

    adding task 3 q3 and q4

---
+++ assignment 1/task2_3sc.py
@@ -0,0 +1,9 @@
+'''3.
+Write a Python program to reverse a word after accepting the input from the user.
+Sample Output:
+Input word: AcadGild
+Output: dilGdacA'''
+
+word=input("Enter the word:")
+reverse_word=word[::-1]
+print(reverse_word)
