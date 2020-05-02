4e7350d18614ba785702eb0e6ff8cee42626e326
Monica Maibram <maibram.monica06@gmail.com>
2020-05-02 13:19:43 +0530

    adding task 2 q1

---
+++ assignment 1/task2_1sc.py
@@ -0,0 +1,7 @@
+'''1.
+Write a program which accepts a sequence of comma-separated numbers from console and
+generate a list.'''
+
+num=input("Enter comma separated numbers:")
+new_num=num.split(',')
+print(new_num)
